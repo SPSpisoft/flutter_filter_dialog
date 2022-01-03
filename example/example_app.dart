2a9451cdeb22ec27b01214439892c80fb26077e6
SPSpisoft <spring_software@yahoo.com>
2022-01-03 16:18:42 +0330

    change anythings

---
+++ example/example_app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class ExampleApp extends StatefulWidget {
+  const ExampleApp({Key? key}) : super(key: key);
+
+  @override
+  _ExampleAppState createState() => _ExampleAppState();
+}
+
+class _ExampleAppState extends State<ExampleApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
